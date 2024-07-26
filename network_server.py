e6f3f53dc72b7e4e842a273a53e9631ee2dd28a7
angie <agpinedam@unal.edu.co>
2024-07-26 10:46:20 +0200

    Proyect finish

--- network_server.py
+++ network_server.py
@@ -2,14 +2,37 @@ import socket
 import threading
 import datetime
 
+FORMAT_CONVERSIONS = {
+    'YYYY-MM-DD HH:mm:SS': '%Y-%m-%d %H:%M:%S',
+    'YYYY/MM/DD HH:mm:SS': '%Y/%m/%d %H:%M:%S',
+    'DD-MM-YYYY HH:mm:SS': '%d-%m-%Y %H:%M:%S',
+    'DD/MM/YYYY HH:mm:SS': '%d/%m/%Y %H:%M:%S',
+    'YYYY-MM-DD HH:mm': '%Y-%m-%d %H:%M',
+    'DD-MM-YYYY HH:mm': '%d-%m-%Y %H:%M',
+    'YYYY-MM-DD': '%Y-%m-%d',
+    'DD-MM-YYYY': '%d-%m-%Y',
+    'HH:mm:SS': '%H:%M:%S',
+    'HH:mm': '%H:%M'
+}
+
+def convert_format(custom_format):
+    return FORMAT_CONVERSIONS.get(custom_format, None)
+
+def validate_format(format_str):
+    return convert_format(format_str) is not None
+
 def handle_client(client_socket, addr):
     print(f"Accepted connection from {addr}")
     try:
         data = client_socket.recv(1024).decode('utf-8').strip()
         if data:
             print(f"Received request: '{data}'")
-            current_time = datetime.datetime.now().strftime(data)
-            response = f"{current_time}\n"
+            converted_format = convert_format(data)
+            if converted_format:
+                current_time = datetime.datetime.now().strftime(converted_format)
+                response = f"{current_time}\n"
+            else:
+                response = "Invalid format\n"
             client_socket.send(response.encode('utf-8'))
             print(f"Sending response: '{response.strip()}'")
         client_socket.close()
